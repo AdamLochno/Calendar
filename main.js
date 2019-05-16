5d7d225d878aa82a55567f05d2e48e204758da49
Adam <34002561+AdamLochno@users.noreply.github.com>
2019-05-17 00:03:45 +0200

    Update and rename newCallendar.js to main.js

---
+++ main.js
@@ -0,0 +1,246 @@
+let today = new Date();
+let currentDay = today.getDay();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let chooseYear = document.getElementById("chooseYear");
+let chooseMonth = document.getElementById("chooseMonth");
+
+
+//pobranie DOM gdzie będą dni wyświetlane
+const tbl = document.getElementById("calendar-body");
+//pobranie przycisków
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
+
+
+
+//Funkcje testowania
+function test() {
+    console.log(`działa`);
+}
+
+
+//Funkcja pójścia do wybranej daty
+function goTo() {
+    currentYear = parseInt(chooseYear.value);
+    currentMonth = parseInt(chooseMonth.value);
+    monthCalendar(currentMonth, currentYear);
+}
+//Funkcja pójścia do następnego miesiąca
+function nextMonth() {
+    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    monthCalendar(currentMonth, currentYear);
+}
+//Funkcja pójścia do poprzedniego miesiąca
+function previousMonth() {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+    monthCalendar(currentMonth, currentYear);
+}
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+let monthAndYear = document.getElementById("monthAndYear");
+
+
+function monthCalendar(month, year) {
+    //sprawdzenie jaki dzień miesiąca jest pierwszy
+    let firstDay = (new Date(year, month)).getDay();
+    //sprawdzenie liczby dni w miesiącu
+    let daysInMonth = new Date(year, month, 0).getDate();
+
+    // wyczyszczenie tablicy
+    tbl.innerHTML = "";
+
+    //wrzucenie informacji o aktualnym przeglądanym miesiącu do nagłówka
+    monthAndYear.innerHTML = months[month] + " " + year;
+
+    //Tworzenie komórek
+    let date = 1;
+    // Stworzenie wierszy
+    for (let i = 0; i < 6; i++) {
+
+        let row = document.createElement("tr");
+
+        //Stworzenie komórek i uzupełnienie ich o odpowiadający dzień tygodnia
+        for (let j = 0; j < 7; j++) {
+            //jeśli dzień będzie mniejszy niż dzień poczatkowy to zostawi program pustą komórkę
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                //wrzucenie danych(cellText) do rodzica (cell)
+                cell.appendChild(cellText);
+                //wrzucenie komórki(cell) do rodzica (row)
+                row.appendChild(cell);
+            }
+            //jeśli iterowana data będzie większa niż liczba dni w miesiącu to pętla zostanie przerwana
+            else if (date > daysInMonth) {
+                break;
+            }
+            //wstawienie komórek z dniami miesiąca do kalendarza
+            else {
+                let cell = document.createElement("td");
+                cell.setAttribute("class", "cell")
+                let cellText = document.createTextNode(date);
+                //stworzenie pola na zadania
+                let note = document.createElement("div");
+
+                note.setAttribute("class", "note");
+                //wrzucenie danych(cellText) do rodzica (cell)
+                cell.appendChild(cellText);
+                //wrzucenie danych(note) do rodzica (cell)
+                cell.appendChild(note);
+                //komórka note zawierać będzie teraz w sobie div + ul
+                note.innerHTML = "<div id='taskField'></div>" + "<ul></ul>";
+                //zdarzenie gdy następuje dwuklik
+                cell.addEventListener("dblclick", test);
+                //wrzucenie komórki(cell) do rodzica(row)
+                row.appendChild(cell);
+                date++;
+            }
+
+        }
+        //wrzucenie rzędu(row) do rodzica (tbl)
+        tbl.appendChild(row);
+    }
+}
+//obsługa przycisków
+next.addEventListener("click", nextMonth);
+previous.addEventListener("click", previousMonth);
+chooseMonth.addEventListener("change", goTo);
+chooseYear.addEventListener("change", goTo);
+
+//const note = document.getElementById("task");
+
+//Metoda zmiany widoku
+function changeView() {
+    let chooseView = document.getElementById('view1').value;
+    console.log(chooseView);
+    if (chooseView == 1) {
+        //gdy widok WEEK
+    } else if (chooseView == 2) {
+        //gdy widok MONTH
+    } else if (chooseView == 3) {
+        //gdy widok YEAR
+    } else {
+        //gdy widok DAY
+    }
+}
+
+//wywołanie funkcji monthCalendar z danymi currentMonth i currentYear
+monthCalendar(currentMonth, currentYear);
+
+
+//dodawanie zadań
+
+const toDoList = [];
+const form = document.getElementById('inputTask');
+const listOfTask = document.getElementById('listOfTask');
+const input = document.querySelector('input');
+// const taskObjectConstructor = function (title, day, month, year) {
+//     this.title = titleTask;
+//     this.day = day;
+//     this.month = month;
+//     this.year = year;
+// }
+
+
+
+
+//metoda dodająca zadanie
+function addTask(e) {
+    e.preventDefault();
+    //pobranie wybranej daty
+    const taskOfDay = document.getElementById('taskDay');
+    const taskOfMonth = document.getElementById('taskMonth');
+    const taskOfYear = document.getElementById('taskYear');
+
+    //przypisanie pobranych wartości daty do zmiennych
+    let dayOfDoingTask = taskOfDay.value;
+    let monthOfDoingTask = taskOfMonth.value;
+    let yearOfDoingTask = taskOfYear.value;
+
+
+    //console.log(input.value);
+    const titleTask = input.value;
+    //zabezpieczenie przed dodawaniem pustych zadań
+    if (titleTask === "") {
+        return;
+    }
+
+    //stworzenie obiektu taskObject
+    // let taskObject = new taskObjectConstructor
+
+    // //stworzenie zadania i dodanie do listy
+    const task = document.createElement('li');
+    task.className = 'task';
+
+    task.innerHTML = titleTask + "<button id='deleteButton'>Usuń</button>";
+    //dodanie taska do tablicy
+    toDoList.push(task);
+
+
+    listOfTask.appendChild(task);
+    input.value = "";
+}
+
+form.addEventListener('submit', addTask);
+
+
+//test jQuery
+window.onload = function () {
+    document.getElementById('calendar-body').onclick = function (e) {
+        var e = e || window.event;
+        var target = e.target || e.srcElement;
+        if (target.tagName.toLowerCase() == "td") {
+            let txt = target.innerHTML;
+            var numb = txt.match(/\d/g);
+            numb = numb.join("");
+
+            const taskOfDay = document.getElementById('taskDay');
+            taskOfDay.value = numb;
+
+            const taskOfMonth = document.getElementById('taskMonth');
+            taskOfMonth.value = currentMonth;
+
+            const taskOfYear = document.getElementById('taskYear');
+            taskOfYear.value = currentYear;
+            console.log(numb);
+            console.log(currentMonth);
+            console.log(currentYear);
+        }
+    };
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Zabawa z Local Storage
+// localStorage.setItem('date', '23.05.1994');
+// console.log(localStorage.getItem('date'));
+
+// let today = new Date();
+// let example = {
+//     title: 'nowy',
+//     date: today.getDate()
+// };
+// localStorage.setItem('object', JSON.stringify(example));
+// console.log(JSON.parse(localStorage.getItem('object')));
